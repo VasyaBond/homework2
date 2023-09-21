a3288e3df3ccad1243bd8f501c330378bf7d1706
Jeka <e.g.bondarev@gmail.com>
2023-09-21 18:57:14 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        if (!int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine("Ошибка ввода. Введите целое число.");
+            return;
+        }
+
+        bool isEven = number % 2 == 0;
+
+        if (isEven)
+        {
+            Console.WriteLine("Число является четным.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является четным.");
+        }
+    }
+}
